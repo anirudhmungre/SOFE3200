e37f1de656fb7508fcd0d05d713566ff7108dcf4
Anirudh Mungre <anirudh.mungre16@gmail.com>
2018-11-19 20:07:38 -0500

    working

--- A2/duplicate.c
+++ A2/duplicate.c
@@ -21,31 +21,31 @@ int main(int argc, char *argv[])
         printf("Not enough args!\n");
         return 0;
     }
-
-    // Open file to read 
-    in = fopen(argv[1], "r");
-    if(in == NULL){
-        perror("File Not Found");
-        return 0;
-    }
-    // Find bytes in file
-    fseek(in, 0L, SEEK_END);
-    numbytes = ftell(in);
-    // Reset file posistion to beginning of file
-    fseek(in, 0L, SEEK_SET);	
     
-    if ((pid = fork()) < 0){
+    if ((pid = fork()) < 0){ // CHeck that fork works
         perror("Error forking");
         exit(1);
     }
-    else if (pid == 0){
+    else if (pid == 0){ // Parent
         while(1) {
-            sleep(1);
+            sleep(1); // Waits every second to print
             printf("still duplicating...\n");
         }
         return 0;
     }
-    else{
+    else{ // Child
+        // Open file to read 
+        in = fopen(argv[1], "r");
+        if(in == NULL){
+            perror("File Not Found");
+            return 0;
+        }
+
+        // Find bytes in file
+        fseek(in, 0L, SEEK_END);
+        numbytes = ftell(in);
+        // Reset file posistion to beginning of file
+        fseek(in, 0L, SEEK_SET);	
         // Create buffer
         buffer = (char*)calloc(numbytes, sizeof(char));	
         // Copy text into a buffer
@@ -57,9 +57,9 @@ int main(int argc, char *argv[])
         // Print to the output file
         fprintf(out, "%s", buffer);
         fclose(out);
-        kill(pid, SIGQUIT);
-        printf("Copied %ld bytes from %s to %s.\n", numbytes, argv[1], argv[2]);
-        exit(1);
+        kill(pid, SIGQUIT); // Send signal to kill parent once done
+        printf("Copied %ld bytes from %s to %s.\n", numbytes, argv[1], argv[2]); // Output requested
+        return 0; //End program
     }
     return 0;
 }
